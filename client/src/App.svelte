1b39dac4167489c84a780208f40a27996b255ee9
rahulmanjunath <yajat472001@gmail.com>
2021-01-21 21:39:12 +0530

    added scss, and EXPPORT

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -4,14 +4,26 @@
 <main>
 	<h1>Svelte + <span class="exp">ExpressJS!</span></h1>
 	<h2>Running on Port 80 using ExpressJS</h2>
+	<div class="sasstitle">Oh, Scss too, because CSS is as old as your grand ma</div>
 </main>
 
-<style>
+<style lang="scss">
 	main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+		display: grid;
+		place-items: center;
+		.sasstitle{
+			color: bisque;
+			background-color: black;
+			padding: 1rem;
+			width: fit-content;
+			border-radius: 1rem;
+		
+		}
+		
 	}
 
 	.exp{
--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
-const express = require('express');
-const app = express()
-const path = require('path')
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = process.env.EXPPORT || 3000;
+//console.log(port);
 
-app.get('/', (req, res)=>{
-	res.sendFile(path.join(__dirname, '..', 'client', 'public', 'index.html'))
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client", "public", "index.html"));
+});
 
-
-app.listen(80, err=>{
-	if(err){ console.log('server cannot listen'); return}
-	console.log('server listening')
-})
+const server = app.listen(port, (err) => {
+  if (err) {
+    console.log("server cannot listen");
+    return;
+  }
+  console.log(
+    `[EXPRESS@${server.address().address}:${
+      server.address().port
+    }] Online and listening`
+  );
+});
